57b930af5427643ddb3e9b72b6ed01943a3a6dcd
canbekcan <can@canbekcan.com>
2026-09-11 17:04:07 +0300

    Regorganize

--- assets/javascripts/discourse/api-initializers/localized-user-fields.js
+++ assets/javascripts/discourse/api-initializers/localized-user-fields.js
@@ -5,15 +5,19 @@ import I18n from 'discourse-i18n';
 export default apiInitializer('1.8.0', (api) => {
   api.modifyClass('model:user-field', {
     pluginId: 'bekcan-localized-user-fields',
+    
     get name() {
       const originalName = this._super(...arguments);
-      const key = originalName ? originalName.toLowerCase().replace(/\s+/g, '_') : '';
+      if (!originalName) return originalName;
+      const key = originalName.toLowerCase().replace(/[^a-z0-9]+/g, '_').replace(/^_+|_+$/g, '');
       return I18n.t(`bekcan.user_fields.${key}.name`, { defaultValue: originalName });
     },
+
     get description() {
-      const originalDesc = this._super(...arguments);
-      const key = this.get('name') ? this.get('name').toLowerCase().replace(/\s+/g, '_') : '';
-      return I18n.t(`bekcan.user_fields.${key}.description`, { defaultValue: originalDesc });
+      const originalName = this.name;
+      if (!originalName) return this._super(...arguments);
+      const key = originalName.toLowerCase().replace(/[^a-z0-9]+/g, '_').replace(/^_+|_+$/g, '');
+      return I18n.t(`bekcan.user_fields.${key}.description`, { defaultValue: this._super(...arguments) });
     }
   });
 });
--- plugin.rb
+++ plugin.rb
@@ -1,14 +1,60 @@
-# plugins/discourse-bekcan-localized-user-fields/plugin.rb
+# name: discourse-localized-user-fields
+# about: Automatic multi-language institutional affiliation based on email domain
+# version: 3.0.0
+# authors: Can Bekcan
+
+# frozen_string_literal: true
+
+enabled_site_setting :localized_user_fields_enabled
+
+require_relative 'lib/localized_user_fields/engine'
+
 after_initialize do
-  module ::BekcanLocalizedUserFields
-    def name
-      I18n.t("bekcan.user_fields.#{super.parameterize.underscore}.name", default: super)
+  next unless SiteSetting.localized_user_fields_enabled
+
+  # Yardımcı metot: Domain'e göre çevrilmiş kurum adını bulur
+  module ::BekcanAffiliationHelper
+    def self.resolve_for(email)
+      return nil if email.blank?
+      domain = email.split('@').last.to_s.downcase
+      
+      # I18n üzerinden mevcut dildeki karşılığını arıyoruz
+      translation_key = "bekcan.institutions.#{domain}"
+      translated_name = I18n.t(translation_key, default: '')
+      
+      translated_name.present? ? translated_name : nil
     end
 
-    def description
-      I18n.t("bekcan.user_fields.#{name.parameterize.underscore}.description", default: super)
+    def self.update_user_affiliation(user)
+      return unless user.present?
+      
+      # 'Affiliation' adlı kullanıcı alanının ID'sini buluyoruz
+      field = UserField.find_by("LOWER(name) = ? OR LOWER(name) = ?", "affiliation", "kurum / üniversite")
+      return unless field.present?
+
+      primary_email = user.user_emails.find_by(primary: true)&.email || user.email
+      institution_name = resolve_for(primary_email)
+
+      if institution_name.present?
+        # Kurum otomatik eşleştiyse kullanıcı alanına yazıyoruz
+        custom_fields = user.custom_fields || {}
+        field_key = "user_field_#{field.id}"
+        
+        if custom_fields[field_key] != institution_name
+          user.custom_fields[field_key] = institution_name
+          user.save_custom_fields
+        end
+      end
     end
   end
 
-  UserField.prepend(::BekcanLocalizedUserFields)
+  # 1. Yeni kullanıcı kaydolduğunda tetiklenir
+  on(:user_created) do |user|
+    ::BekcanAffiliationHelper.update_user_affiliation(user)
+  end
+
+  # 2. Kullanıcı e-postası değiştiğinde tetiklenir
+  on(:user_emails_changed) do |user|
+    ::BekcanAffiliationHelper.update_user_affiliation(user)
+  end
 end
